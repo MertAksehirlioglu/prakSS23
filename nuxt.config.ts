4070434a7c8c358c0210c89411f5c925dcbd5548
mertaksehirlioglu <mert.aksehirlioglu@tum.de>
2023-07-17 23:05:01 +0200

    Gitignore and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  app: {
+    baseURL: '/~ge75cos/public/'
+  },
   devtools: { enabled: true },
   css: ['vuetify/lib/styles/main.sass'],
     build: {
